0bf65befd8c797ba62fcb5b141748504d0e80318
ritvikbansal-codes <ritvikbansal903@gmail.com>
2026-09-10 19:24:29 +0530

    Add files via upload

---
+++ Day 1_2nd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%f", &num2);
+
+    printf("Sum = %.2f\n", num1 + num2);
+    printf("Difference = %.2f\n", num1 - num2);
+    printf("Product = %.2f\n", num1 * num2);
+
+    if (num2 != 0)
+        printf("Quotient = %.2f\n", num1 / num2);
+    else
+        printf("Quotient = undefined (division by zero)\n");
+
+    return 0;
+}
